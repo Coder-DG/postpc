9c47a6d00d283a18c95da167f25b01a31f36e6e6
David Ginzbourg <davidginzbourg@gmail.com>
2019-05-08 20:06:37 +0300

    Finish implementing

--- app/src/main/java/com/dginzbourg/postpc/DemoActivity1.kt
+++ app/src/main/java/com/dginzbourg/postpc/DemoActivity1.kt
@@ -4,16 +4,31 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class DemoActivity1 : AppCompatActivity() {
+
+    lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo1)
 
-        val goFishButton = findViewById<Button>(R.id.goFishButton)
-        goFishButton.setOnClickListener {
+        findViewById<Button>(R.id.go_fish_button).setOnClickListener {
             startActivity(Intent(this, DemoActivity2::class.java))
+        }
+
+        textView = findViewById<TextView>(R.id.tap_on_me)
+        textView.setOnClickListener {
+            val intent = Intent(this, DemoActivity3::class.java)
+            startActivityForResult(intent, 1234)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1234 && resultCode == RESULT_OK) {
+            val res = "you have entered: ${data?.getStringExtra("result")}"
+            textView.text = res
         }
     }
 }
--- app/src/main/java/com/dginzbourg/postpc/DemoActivity3.kt
+++ app/src/main/java/com/dginzbourg/postpc/DemoActivity3.kt
@@ -1,12 +1,34 @@
 package com.dginzbourg.postpc
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 
 class DemoActivity3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo3)
+
+        findViewById<EditText>(R.id.digits_edit_text).addTextChangedListener(
+                object : TextWatcher {
+                    override fun afterTextChanged(s: Editable?) {
+                        if (s?.length == 5) {
+                            val intent = Intent(this@DemoActivity3, DemoActivity1::class.java)
+                            intent.putExtra("result", s.toString())
+                            setResult(Activity.RESULT_OK, intent)
+                        }
+                    }
+
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    }
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    }
+                })
     }
 }
