1173a682f3a85c5f0415e6f8fe209ef1406a0545
David Ginzbourg <davidginzbourg@gmail.com>
2019-05-14 15:39:45 +0300

    Add new message details activity

---
+++ app/src/main/java/com/dginzbourg/postpc/MessageDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dginzbourg.postpc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_detail)
+    }
+}
