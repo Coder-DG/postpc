405dc1ad3804c4a38ec19fb214700663aab94225
David Ginzbourg <davidginzbourg@gmail.com>
2019-05-22 20:00:36 +0300

    Add username check functionality

--- app/src/main/java/com/dginzbourg/postpc/LoginActivity.kt
+++ app/src/main/java/com/dginzbourg/postpc/LoginActivity.kt
@@ -8,18 +8,70 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     private val permissionArray = arrayOf(
         Manifest.permission.INTERNET
     )
+    private lateinit var usernameEditText: EditText
+    private lateinit var setUsernameButton: Button
+    private val usernameRegex = "[a-zA-Z0-9]+".toRegex()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         validatePermissionsGranted()
+
+        setUsernameButton = findViewById(R.id.loginSetUsername)
+        usernameEditText = findViewById(R.id.loginUsername)
+        loadUserEditTextAttributes(savedInstanceState)
+        usernameEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.isEmpty() == true) {
+                    usernameEditText.error = getString(R.string.usernameRequired)
+                } else if (s != null && !usernameRegex.matches(s)) {
+                    usernameEditText.error = getString(R.string.usernameError)
+                } else {
+                    usernameEditText.error = null
+                }
+
+                setUsernameButton.isEnabled = usernameEditText.error == null
+            }
+
+        })
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        loadUserEditTextAttributes(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.apply {
+            putString(USERNAME_TEXT, usernameEditText.text.toString())
+            putString(USERNAME_ERROR_KEY, usernameEditText.error?.toString())
+        }
+    }
+
+    private fun loadUserEditTextAttributes(savedInstanceState: Bundle?) {
+        savedInstanceState?.let {
+            usernameEditText.setText(it.getString(USERNAME_TEXT, ""))
+            usernameEditText.error = it.getString(USERNAME_ERROR_KEY)
+            setUsernameButton.isEnabled = usernameEditText.error == null
+        }
     }
 
     private fun validatePermissionsGranted() {
@@ -68,4 +120,8 @@ class LoginActivity : AppCompatActivity() {
         return builder.create()
     }
 
+    companion object {
+        const val USERNAME_ERROR_KEY = "username_error"
+        const val USERNAME_TEXT = "username_text"
+    }
 }
