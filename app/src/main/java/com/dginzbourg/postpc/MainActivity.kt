9941eb1be683762b8f0650116b467173ed8d9221
David Ginzbourg <davidginzbourg@gmail.com>
2019-05-24 12:55:36 +0300

    Implement token request

--- app/src/main/java/com/dginzbourg/postpc/MainActivity.kt
+++ app/src/main/java/com/dginzbourg/postpc/MainActivity.kt
@@ -1,26 +1,62 @@
 package com.dginzbourg.postpc
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    internal data class UserInfo(val picURL: String, val prettyName: String)
+
+    private var token = MutableLiveData<String>()
+    private var userInfo = MutableLiveData<UserInfo>()
     private lateinit var username: String
     private lateinit var profilePic: ImageView
     private lateinit var usernameTextView: TextView
     private lateinit var prettyNameEditText: EditText
     private lateinit var updatePrettyNameButton: Button
+    private lateinit var requestQueue: RequestQueue
+    private val executor = Executors.newCachedThreadPool()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initUIElements()
-        if(!restoreUITexts(savedInstanceState)){
+        if (!restoreUITexts(savedInstanceState)) {
             usernameTextView.text = intent.getStringExtra(DB_USERNAME_KEY)
         }
+        fetchToken()
+        token.observe(this, Observer { fetchUserInfo() })
+    }
+
+    private fun fetchToken() {
+        requestQueue = Volley.newRequestQueue(application)
+        showToast(this, getString(R.string.loading), Toast.LENGTH_LONG)
+        val stringRequest = JsonObjectRequest(
+            "$SERVER_BASE_URL$SERVER_USERS_URL$username/$SERVER_TOKEN_URL",
+            null,
+            Response.Listener<JSONObject> {
+                token.postValue(it[REQUESTS_DATA_KEY] as String)
+            },
+            Response.ErrorListener {
+                showToast(this, getString(R.string.something_went_wrong), Toast.LENGTH_LONG)
+            }).also {
+            it.tag = this
+        }
+
+        requestQueue.add(stringRequest)
+    }
+
+    private fun fetchUserInfo() {
+
     }
 
     private fun initUIElements() {
@@ -59,5 +95,12 @@ class MainActivity : AppCompatActivity() {
         const val USERNAME = "username"
         const val PRETTY_NAME = "pretty_name"
         const val UPDATE_BUTTON_IS_ENABLED = "update_button_is_enabled"
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (this::requestQueue.isInitialized)
+            requestQueue.cancelAll(this)
+        executor.shutdown()
     }
 }
--- app/src/main/java/com/dginzbourg/postpc/Utils.kt
+++ app/src/main/java/com/dginzbourg/postpc/Utils.kt
@@ -4,9 +4,15 @@ import android.content.Context
 import android.widget.Toast
 
 internal const val SERVER_BASE_URL = "http://hujipostpc2019.pythonanywhere.com/"
+internal const val SERVER_USERS_URL = "users/"
+internal const val SERVER_TOKEN_URL = "token/"
+internal const val SERVER_USER_URL = "user/"
+internal const val SERVER_EDIT_URL = "edit/"
 internal const val SHARED_PREFERENCES_FILE = "com.dginzbourg.postpc_ex8"
 internal const val DB_USERNAME_KEY = "username"
 internal const val REQUESTS_DATA_KEY = "data"
+internal const val REQUESTS_PRETTY_NAME_KEY = "pretty_name"
+internal const val REQUESTS_IMAGE_URL_KEY = "image_url"
 
 fun showToast(context: Context, message: String, duration: Int) {
     val toast = Toast.makeText(context, message, duration)
