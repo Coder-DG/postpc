ded2539ecfa452d4a00ad1f02e40a6599a021450
David Ginzbourg <davidginzbourg@gmail.com>
2019-05-24 13:37:11 +0300

    Implement user info fetch

--- app/src/main/java/com/dginzbourg/postpc/MainActivity.kt
+++ app/src/main/java/com/dginzbourg/postpc/MainActivity.kt
@@ -4,6 +4,8 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.*
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -13,10 +15,9 @@ import org.json.JSONObject
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    internal data class UserInfo(val picURL: String, val prettyName: String)
-
     private var token = MutableLiveData<String>()
-    private var userInfo = MutableLiveData<UserInfo>()
+    private var picURL = MutableLiveData<String>()
+    private var prettyName = MutableLiveData<String>()
     private lateinit var username: String
     private lateinit var profilePic: ImageView
     private lateinit var usernameTextView: TextView
@@ -38,8 +39,33 @@ class MainActivity : AppCompatActivity() {
         if (!restoreUITexts(savedInstanceState)) {
             usernameTextView.text = intent.getStringExtra(DB_USERNAME_KEY)
         }
+        setupListeners()
         fetchToken()
+    }
+
+    private fun setupListeners() {
+        prettyNameEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val isTextValid = s?.isNotBlank() == true
+                updatePrettyNameButton.isEnabled = isTextValid && prettyName.value != s.toString()
+                prettyNameEditText.error = when (isTextValid) {
+                    false -> getString(R.string.invalid_pretty_name)
+                    else -> null
+                }
+            }
+
+        })
         token.observe(this, Observer { fetchUserInfo() })
+        picURL.observe(this, Observer {
+            // TODO use Glide to inflate the image
+        })
+        prettyName.observe(this, Observer { prettyNameEditText.setText(prettyName.value) })
     }
 
     private fun showErrorToast() {
@@ -86,12 +112,8 @@ class MainActivity : AppCompatActivity() {
                     showErrorToast()
                     return@Listener
                 }
-                userInfo.postValue(
-                    UserInfo(
-                        data[REQUESTS_PRETTY_NAME_KEY] as String,
-                        data[REQUESTS_IMAGE_URL_KEY] as String
-                    )
-                )
+                picURL.postValue(data[REQUESTS_IMAGE_URL_KEY] as String)
+                prettyName.postValue(data[REQUESTS_PRETTY_NAME_KEY] as String)
             },
             errorListener
         ).also {
@@ -114,6 +136,7 @@ class MainActivity : AppCompatActivity() {
             username = getString(USERNAME, null)
             usernameTextView.text = username
             prettyNameEditText.setText(getString(PRETTY_NAME, "Error!"))
+            prettyNameEditText.error = getString(PRETTY_NAME_ERROR, null)
             updatePrettyNameButton.isEnabled = getBoolean(UPDATE_BUTTON_IS_ENABLED, false)
             return true
         }
@@ -130,6 +153,7 @@ class MainActivity : AppCompatActivity() {
         outState?.apply {
             putString(USERNAME, username)
             putString(PRETTY_NAME, prettyNameEditText.text.toString())
+            putString(PRETTY_NAME_ERROR, prettyNameEditText.error?.toString())
             putBoolean(UPDATE_BUTTON_IS_ENABLED, updatePrettyNameButton.isEnabled)
         }
     }
@@ -137,6 +161,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val USERNAME = "username"
         const val PRETTY_NAME = "pretty_name"
+        const val PRETTY_NAME_ERROR = "pretty_name_error"
         const val UPDATE_BUTTON_IS_ENABLED = "update_button_is_enabled"
     }
 
